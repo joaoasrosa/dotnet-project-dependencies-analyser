0e4198bf2dad718b5e0d533466dedf36b007da73
joaoasrosa <joaoasrosa@gmail.com>
2018-04-02 20:15:27 +0200

    Write the report.

--- src/Cake.DependenciesChecker/DependenciesCheckerAliases.cs
+++ src/Cake.DependenciesChecker/DependenciesCheckerAliases.cs
@@ -17,31 +17,48 @@ namespace Cake.DependenciesChecker
         /// <param name="settings">The settings.</param>
         [CakeMethodAlias]
         public static void AnalyseDependencies(
-            this ICakeContext context, 
+            this ICakeContext context,
             DependenciesCheckerSettings settings)
         {
-            if(context == null)
+            if (context == null)
                 throw new ArgumentNullException(nameof(context));
 
-            if(settings == null)
+            if (settings == null)
                 throw new ArgumentNullException(nameof(settings));
 
             var analyseDependencies = new AnalyseDependencies(
                 new AnalyserServicesFactory(
                     new NugetDependencyChecker(
                         new HttpClient()
-                        )
                     )
-                );
+                )
+            );
 
             var report = analyseDependencies.Execute(
                 new AnalyseDependenciesSettings(
-                    string.IsNullOrWhiteSpace(settings.Project) ? (File?)settings.Project : null,
-                    string.IsNullOrWhiteSpace(settings.Folder) ? (Folder?)settings.Folder : null
-                    )
-                );
-            
-            // TODO: how to display the report?
+                    string.IsNullOrWhiteSpace(settings.Project) ? (File?) settings.Project : null,
+                    string.IsNullOrWhiteSpace(settings.Folder) ? (Folder?) settings.Folder : null
+                )
+            );
+
+            foreach (var reportProjectDependency in report.ProjectDependencies)
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine(
+                    "Project: {0}",
+                    reportProjectDependency.Project);
+                
+                foreach (var dependencyReport in reportProjectDependency.Dependencies)
+                    Console.WriteLine(
+                        "{0} is on version {1}. The dependency is {2}.",
+                        dependencyReport.Dependency,
+                        dependencyReport.LatestVersion,
+                        dependencyReport.HasNewerVersion ? "outdated" : "up-to-date"
+                    );
+                
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine();
+            }
         }
     }
 }
