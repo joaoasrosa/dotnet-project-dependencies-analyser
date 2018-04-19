4468013f11941f58cf54f106f306af6951dfebca
joaoasrosa <joaoasrosa@gmail.com>
2018-04-19 19:35:18 +0200

    Update documentation

--- src/Cake.DependenciesAnalyser/DependenciesAnalyserSettings.cs
+++ src/Cake.DependenciesAnalyser/DependenciesAnalyserSettings.cs
@@ -2,7 +2,14 @@
 {
     public class DependenciesAnalyserSettings
     {
+        /// <summary>
+        ///     Gets or sets a value for the folder to scan for C# projects for analysis.
+        /// </summary>
         public string Folder { get; set; }
+
+        /// <summary>
+        ///     Gets or sets a value for the Project for analysis.
+        /// </summary>
         public string Project { get; set; }
     }
 }
