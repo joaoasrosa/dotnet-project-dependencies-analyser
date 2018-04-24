9b574aee17516e195d3922090dfa55b18f3a259e
joaoasrosa <joaoasrosa@gmail.com>
2018-04-24 21:43:41 +0200

    Use NuGet.Command package

--- tests/Domain.Tests.Unit/Given.cs
+++ tests/Domain.Tests.Unit/Given.cs
@@ -10,29 +10,11 @@ namespace Domain.Tests.Unit
         internal static DependencyCheckerBuilder DependencyChecker =>
             new DependencyCheckerBuilder();
 
-        internal static HttpClientBuilder HttpClient =>
-            new HttpClientBuilder();
-
-        internal static HttpMessageHandlerBuilder HttpMessageHandler =>
-            new HttpMessageHandlerBuilder();
-
         internal static AnalyseDependenciesSettingsBuilder AnalyseDependenciesSettings =>
             new AnalyseDependenciesSettingsBuilder();
 
-        internal static HttpResponseMessageBuilder HttpResponseMessage =>
-            new HttpResponseMessageBuilder();
-
         internal static DependencyBuilder Dependency =>
             new DependencyBuilder();
-
-        internal static NugetResponseBuilder NugetResponse =>
-            new NugetResponseBuilder();
-
-        internal static NugetResponseDataBuilder NugetResponseData =>
-            new NugetResponseDataBuilder();
-
-        internal static UriBuilder Uri =>
-            new UriBuilder();
 
         internal static CsProjectBuilder CsProject =>
             new CsProjectBuilder();
--- tests/Domain.Tests.Unit/WhenAnalysingProject.cs
+++ tests/Domain.Tests.Unit/WhenAnalysingProject.cs
@@ -10,24 +10,9 @@ namespace Domain.Tests.Unit
 {
     public class WhenAnalysingProject
     {
-        private static AnalyseDependencies CreateSut(
-            params Tuple<Uri, HttpResponseMessage>[] httpResponseMessages)
+        private static AnalyseDependencies CreateSut()
         {
-            var httpMessageHandlerBuilder = Given.HttpMessageHandler;
-
-            foreach (var httpResponseMessage in httpResponseMessages)
-                httpMessageHandlerBuilder.WithHttpResponseMessageForUri(
-                    httpResponseMessage.Item2,
-                    httpResponseMessage.Item1);
-
-            var httpMessageHandler = httpMessageHandlerBuilder.Build();
-
-            var httpClient = Given.HttpClient
-                .WithHttpMessageHandler(httpMessageHandler)
-                .Build();
-
             var dependencyChecker = Given.DependencyChecker
-                .WithHttpClient(httpClient)
                 .Build();
 
             var analyserServicesFactory = Given.AnalyserServicesFactory
@@ -35,40 +20,6 @@ namespace Domain.Tests.Unit
                 .Build();
 
             return new AnalyseDependencies(analyserServicesFactory);
-        }
-
-        private static Tuple<Uri, HttpResponseMessage> CreateHttpResponseMessageTuple(
-            Name name,
-            SemVersion version)
-        {
-            var dependency = Given.Dependency
-                .WithName(name)
-                .WithVersion(version)
-                .Build();
-
-            var nugetResponseData = Given.NugetResponseData
-                .WithId(dependency.Name)
-                .WithVersion(dependency.Version.ToString())
-                .Build();
-
-            var nugetResponse = Given.NugetResponse
-                .WithData(nugetResponseData)
-                .Build();
-
-            var httpResponseMessage = Given.HttpResponseMessage
-                .WithOkAsHttpStatusCode()
-                .WithContent(nugetResponse)
-                .Build();
-
-            var uri = Given.Uri
-                .WithUrl($"https://api-v2v3search-0.nuget.org/query?q={dependency.Name}")
-                .Build();
-
-            var tuple = new Tuple<Uri, HttpResponseMessage>(
-                uri,
-                httpResponseMessage);
-
-            return tuple;
         }
 
         [Fact]
@@ -85,11 +36,7 @@ namespace Domain.Tests.Unit
                 .WithName("Dummy.csproj")
                 .CreateAt(subfolder);
 
-            var jsonPackage = CreateHttpResponseMessageTuple(
-                (Name) "Newtonsoft.Json",
-                SemVersion.Parse("11.0.3"));
-
-            var sut = CreateSut(jsonPackage);
+            var sut = CreateSut();
 
             var result = sut.Execute(settings);
 
